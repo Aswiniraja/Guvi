a6929b84ba127eb0043104b9bc9c728a79672099
Aswiniraja <aswinirajaeee@gmail.com>
2016-07-28 17:37:13 +0530

    Add files via upload

---
+++ customer.java
@@ -0,0 +1,14 @@
+//Source file: E:\\customer.java
+
+//void cashier.saletotal(){
+//    
+//   }
+//void payment.receiptgeneration(){
+//    
+//   }
+//cashier.cashier()
+//customer.customer()
+//void cashier.saletransaction(){
+//    
+//   }
+//payment.payment()
