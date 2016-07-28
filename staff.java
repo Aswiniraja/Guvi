4d72dc0e63ca63b99a199062de10d13988dc725e
Aswiniraja <aswinirajaeee@gmail.com>
2016-07-28 17:39:43 +0530

    Add files via upload

---
+++ staff.java
@@ -0,0 +1,24 @@
+//Source file: E:\\staff.java
+
+
+class staff 
+{
+   public string name;
+   public int regno;
+   
+   /**
+   @roseuid 5704D80001AF
+    */
+   public staff() 
+   {
+    
+   }
+   
+   /**
+   @roseuid 5704D3CB0212
+    */
+   public void dept() 
+   {
+    
+   }
+}
